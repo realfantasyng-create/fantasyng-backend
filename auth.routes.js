856b014c0a90166b8c2231ea6d3a4acc41cf0921
realfantasyng-create <realfantasyng@gmail.com>
2026-04-16 09:11:20 +0100

    Update auth.routes.js

--- auth.routes.js
+++ auth.routes.js
@@ -3,7 +3,7 @@
 // =====================================================
 const express = require('express');
 const router = express.Router();
-const { signup, login, logout, forgotPassword, resetPassword, getMe } = require('./controllers/auth.controller');
+const { signup, login, logout, forgotPassword, resetPassword, getMe } = require('./auth.controller');
 const { protect } = require('./auth.middleware');
 
 router.post('/signup', signup);
